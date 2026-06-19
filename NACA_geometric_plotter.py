a62d62558b7b350821d03f9f57028a098becaa43
Sidd Somadder <ss18207@nyu.edu>
2026-06-18 23:36:57 -0400

    importing libraries for computation, plotting, and data exporting

--- NACA_geometric_plotter.py
+++ NACA_geometric_plotter.py
@@ -1,4 +1,6 @@
 # This is the script for NACA 4-series and 5-series airfoil plotting. 
 # The geometric data points of the airfoil are exported in (x,y) coordinates in a .csv file
 
-
+import numpy as np
+import pandas as pd
+import matplotlib as plt
