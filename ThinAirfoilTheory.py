995b12805ac3be0909378015a1edbde415307304
Sidd Somadder <ss18207@nyu.edu>
2026-06-19 14:51:03 -0400

    Created new thin airfoil theory file with description of script purpose

---
+++ ThinAirfoilTheory.py
@@ -0,0 +1,3 @@
+# This script will run Thin Airfoil Theory derivations and is called automatically by the master script
+# .csv data for the lift, leading edge moment, and quarter-chord moment coefficients against angle of attacks is outputted
+# The chosen .csv file in saved_airfoil_coords folder must have proper naming: "NACA_XXXX_N#.csv" for this script to execute successfully
