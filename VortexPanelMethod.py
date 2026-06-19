991a7ead5e293770153035d0d997586f9a4eca5a
Sidd Somadder <ss18207@nyu.edu>
2026-06-19 04:55:27 -0400

    file creation and made some comments describing script's role in the full project, its expected output, and what it needs to work.

---
+++ VortexPanelMethod.py
@@ -0,0 +1,3 @@
+# This script will run a vortex panel computation method and is called automatically by the master script
+# .csv data for the lift, leading edge moment, and quarter-chord moment coefficients against angle of attacks is outputted
+# Geometric surface points must be in the saved_airfoil_coords folder in order for successful execution
