3ef4bb43c6a5f387c7b6892b1fb68da1c6673881
Sidd Somadder <ss18207@nyu.edu>
2026-06-20 23:11:56 -0400

    Added brief note that TAT script will only handle naca 4-digit series; will implement handling 5 digit series for a future update

--- ThinAirfoilTheory.py
+++ ThinAirfoilTheory.py
@@ -1,3 +1,5 @@
 # This script will run Thin Airfoil Theory derivations and is called automatically by the master script
 # .csv data for the lift, leading edge moment, and quarter-chord moment coefficients against angle of attacks is outputted
 # The chosen .csv file in saved_airfoil_coords folder must have proper naming: "NACA_XXXX_N#.csv" for this script to execute successfully
+
+# Currently, this thin airfoil theory generator can only handle NACA 4-digit series 
