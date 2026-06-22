21c872872f41b10e98dadd03caee5f977ad7d92d
Sidd Somadder <ss18207@nyu.edu>
2026-06-22 04:16:31 -0400

    Implemented simple test for cross-script functoin calls to verify string parsing and provide a simple reference for how master script will handle calling TAT script with a known file name.

--- airfoil_aerodynamic_analysis.py
+++ airfoil_aerodynamic_analysis.py
@@ -1,4 +1,19 @@
 # This is the master script which the user will interact with. The user will never need to open TAT, VPM, or XFOIL scripts.
 # The user will be able to select a saved airfoil coordinates file from the folder and choose to run an aerodynamic analysis
 # The master script will call all three scripts for results and plot the c_l, c_m,LE, and c_m,c/4 values against AOA.
-# The user can choose to save the results of all three scripts in the computation_results folder.  
+# The user can choose to save the results of all three scripts in the computation_results folder.
+
+from ThinAirfoilTheory import run_tat_solver
+
+# Temporary sample file names to test cross-script function calls
+sample_file_name1 = "NACA_4412_N100.dat";
+sample_file_name2 = "Clarky_N60.dat";
+
+# Should print that input is a NACA airfoil
+print(f"Using sample {sample_file_name1} : ")
+run_tat_solver(sample_file_name1);
+print("---");
+
+# Should print that input is not a NACA airfoil
+print(f"Using sample {sample_file_name2} : ")
+run_tat_solver(sample_file_name2);
