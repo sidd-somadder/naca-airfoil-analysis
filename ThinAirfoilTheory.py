53601d569654cdc8bb2949024a2fcc088ac7a0d0
Sidd Somadder <ss18207@nyu.edu>
2026-06-22 04:10:55 -0400

    Wrote simple function to read file name for thin airfoil theory when accessed from master script. Used primarily for testing cross-script function calls.

--- ThinAirfoilTheory.py
+++ ThinAirfoilTheory.py
@@ -3,3 +3,15 @@
 # The chosen .csv file in saved_airfoil_coords folder must have proper naming: "NACA_XXXX_N#.csv" for this script to execute successfully
 
 # Currently, this thin airfoil theory generator can only handle NACA 4-digit series 
+
+# For future implementation: if airfoil coordinates are provided and are not NACA series, 
+# warn user that mean camber line will be approximated numerically which introduces uncertainty and noise to results
+
+# Master 
+def run_tat_solver(input_file_name):
+    target_code = "NACA";
+    print(input_file_name) # Placeholder
+    if target_code in input_file_name:
+        print("This is a NACA code.");
+    else:
+        print("This is not a NACA code.")
