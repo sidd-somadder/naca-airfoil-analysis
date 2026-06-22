578d650960635d90b026098eb5cd71ed15e54aa9
Sidd Somadder <ss18207@nyu.edu>
2026-06-22 03:54:25 -0400

    Commented a brief discription of how the master script will be used by the user and the expected input/output when it is ran.

---
+++ airfoil_aerodynamic_analysis.py
@@ -0,0 +1,4 @@
+# This is the master script which the user will interact with. The user will never need to open TAT, VPM, or XFOIL scripts.
+# The user will be able to select a saved airfoil coordinates file from the folder and choose to run an aerodynamic analysis
+# The master script will call all three scripts for results and plot the c_l, c_m,LE, and c_m,c/4 values against AOA.
+# The user can choose to save the results of all three scripts in the computation_results folder.  
