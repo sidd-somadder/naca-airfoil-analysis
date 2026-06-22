36ac5769fc6b564f08584df0736e97f5b32c7e96
Sidd Somadder <ss18207@nyu.edu>
2026-06-22 05:23:35 -0400

    Created placeholder function to setup master script structure

--- xfoil_wrapper.py
+++ xfoil_wrapper.py
@@ -1,3 +1,6 @@
 # This script will run a subprocess method on the XFOIL software to validate results from the VPM and TAT scripts
 # .csv data for the lift, leading edge moment, and quarter-chord moment coefficients against angle of attacks is outputted
 # Calling this method is expected to yield the most accurate results since it combines VPM with integral boundary layer equations
+
+def run_xfoil_solver(input_file_name):
+    print("Placeholder");
