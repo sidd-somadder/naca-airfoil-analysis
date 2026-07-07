247943abacda92b88c7b4c412c094754a250fdcd
Sidd Somadder <ss18207@nyu.edu>
2026-07-07 17:42:40 -0400

    Implemented a method that computes the local velocity at collocation points, a method that generates the influence coefficients matrix using geometric data and calling the local velocity computer method as helper method. In addition, implemented a helper method to get an array of midpoints for a given set of geometric coordinate points. Implemented a method that returns the right hand side of the Agamma = b matrix equation.

--- VortexPanelMethod.py
+++ VortexPanelMethod.py
@@ -11,27 +11,116 @@ import pandas as pd;
 import os;
 import math;
 
-def run_vpm_solver(input_file_name):
+# Assume points are already processed into reverse Selig format
+def run_vpm_solver(geom_points):
+
     print("Placeholder");
 
-# Method that determines local induced velocity at collocation points (xi, zi) by vortex panels
-def V2DC_induced_vel(loc_gam, x_i, z_i, x_j, z_j, x_jp1, z_jp1):
-    # Note, Katz uses (x,z) for coordinates instead of (x,y), I use the same convention here.
-    # Compute differences between collocation and jth panel node coordinates
-    zdif1 = z_i - z_j;
-    xdif1 = x_i - x_j;
 
-    # Compute differences between collocation and j+1th panel node coordinates
-    zdif2 = z_i - z_jp1
-    xdif2 = x_i - x_jp1
 
-    # Calculate geometric distance between collocation and each of jth and j+1th panels
-    jdist = xdif1**2 + zdif1**2;
-    jp1dist = xdif2**2 + zdif2**2;
+# Method that determines local induced velocity at collocation points (xi, zi) by panel between the jth and j+1th nodes
+def V2DC_induced_vel(loc_gam, x_i, z_i, x_j, z_j, x_jp1, z_jp1, beta_j):
+    # Note, Katz uses (x,z) for coordinates instead of (x,y), I use the same convention here. Note: jp1 = j+1.
+    # First, derive translated coordinates of the ith collocation point
+    x_i_prime = x_i - x_j;
+    z_i_prime = z_i - z_j;
+
+    # Using tangential angle beta of the j to j+1th panel, transform prime coordinates to panel coordinates (bar)
+    x_i_bar = x_i_prime * np.cos(beta_j) + z_i_prime * np.sin(beta_j);
+    z_i_bar = -x_i_prime * np.sin(beta_j) + z_i_prime * np.cos(beta_j);
+
+    # Calculate panel length from panel node coordinates (x_j, z_j) and (x_jp1, z_jp1)
+    p_L = np.sqrt((z_jp1 - z_j)**2 + (x_jp1 - x_j)**2);
 
     # Local induced velocity (Katz eqn. 11.44 & 11.45)
-    u_i = (loc_gam/(2*np.pi))*(np.arctan2(zdif2/xdif2) - np.arctan2(zdif1/xdif1));
-    w_i = (-loc_gam/(4*np.pi))*(np.log(jdist/jp1dist));
+    ubar = loc_gam/(2*np.pi) * (np.arctan2(z_i_bar,(x_i_bar-p_L)) - np.arctan2(z_i_bar,x_i_bar)) 
+    wbar = -loc_gam/(4*np.pi) * np.log(((x_i_bar)**2 + (z_i_bar)**2)/((x_i_bar - p_L)**2 + (z_i_bar)**2))
+    
+    # Use tangential angle again to rotate velocity vector to cartesian coordinates from panel coordinates
+    u_i = ubar * np.cos(beta_j) - wbar * np.sin(beta_j);
+    w_i = ubar * np.sin(beta_j) + wbar * np.cos(beta_j);
     
     # Returns local induced velocity in coordinate form
     return u_i, w_i;
+
+# 
+def V2DC_influence_matrix(geom_pts, midpoints, beta):
+    N = len(geom_pts);
+
+    A = np.zeros((N,N));
+
+    for i in range(N-1):
+        x_i = midpoints[i,0];
+        z_i = midpoints[i,1];
+        for j in range(N):
+            # Define jth x,z coordinates required to calculate (i,j)th influence coefficient
+            x_j = geom_pts[j,0]
+            z_j = geom_pts[j,1]
+
+            # If j = N-1, loop back to the first index;
+            if j+1 == N:
+               x_jp1 = geom_pts[0,0];
+               z_jp1 = geom_pts[0,1];
+            # Otherwise, continue down the array of points
+            else:
+               x_jp1 = geom_pts[j+1,0];
+               z_jp1 = geom_pts[j+1,1];
+            
+            if i == j:
+               # Using Katz eqn. 11.50
+               A[i,j] = -0.5;
+            else:
+               # Calculate singularity vortex element influenced velocities
+               u, w = V2DC_induced_vel(1, x_i, z_i, x_j, z_j, x_jp1, z_jp1, beta[j]);
+               # Using Katz eqn. 11.49
+               A[i,j] = u * np.cos(beta[i]) - w * np.sin(beta[i]);  
+    
+    # We replace the blunt TE panel influence equation with the Kutta Condiiton for the panels adjacent to TE points
+    A[N-1,0] = 1;
+    A[N-1,N-2] = 1;
+
+    return A;
+
+# Returns the RHS for the Ag = RHS matrix equation
+# Takes the set of angle of attacks (alpha) and all local panel tangential angles (beta);
+def V2DC_RHS_vec(alpha, beta):
+    # Define Cartesian components of freestream velocity.
+    # Since this solver returns coefficients instead of raw lift values, assume V = 1 
+    U = np.cos(alpha);
+    W = np.sin(alpha);
+
+    # If there are N tangential angles, then there are N panels, and therefore the RHS has N elements
+    RHS = np.zeros_like(beta);
+
+    # (RHS)_i = - (U,W) dot (cos(beta[i] - sin(beta[i]))); Katz eqn. 11.51
+    RHS = - U * np.cos(beta) + W * np.sin(beta);
+
+    # To include the Kutta Condition for the Nth equation, we set RHS = 0 
+    RHS[-1] = 0.0;
+
+    return RHS;
+
+def get_midpoints(geom_pts):
+
+    # number of midpoints = number of panel nodes for a closed system of panels
+    N = len(geom_pts);
+    midpoints = np.zeros((N,2));
+    
+    # Use the face that the midpoint between two values is their sum divided by 2.
+    for i in range(N):
+        if i < (N-1):
+            x_m = 0.5 *(geom_pts[i,0] + geom_pts[i+1,0]);
+            z_m = 0.5 *(geom_pts[i,1] + geom_pts[i+1,1]);
+            midpoints[i,:] = [x_m, z_m];
+        # if i is on the N-1th index, we consider the 0 as the i+1 index to represent the first panel node
+        else:
+            x_m = 0.5 *(geom_pts[i,0] + geom_pts[0,0]);
+            z_m = 0.5 *(geom_pts[i,1] + geom_pts[0,1]);
+            midpoints[i,:] = [x_m, z_m];
+
+    return midpoints;
+
+
+
+
+
